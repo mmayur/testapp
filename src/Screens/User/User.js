6c096d55e9e1ec3a30833a927f302c2e96232c9f
mmayur <mayurmorey@gmail.com>
2020-05-27 21:49:46 +0530

    converted user component to functional

--- src/Screens/User/User.js
+++ src/Screens/User/User.js
@@ -1,87 +1,86 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { getUsersSaga } from "../../Redux/actions/index.js";
 import CardList from "./CardList";
 import "./styles/style.css";
+import { useSelector, useDispatch } from "react-redux";
 
-class User extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showFiltere: false,
-		};
-	}
-	componentDidMount() {
-		this.props.getUsersSaga();
-	}
+const User = () => {
+	const [showFiltere, setFilter] = React.useState(false);
 
-	filtere = () => {
-		this.setState({ showFiltere: true });
+	const dispatch = useDispatch();
+
+	React.useEffect(() => {
+		dispatch(getUsersSaga());
+	});
+
+	const filtere = () => {
+		//	this.setState({ showFiltere: true });
+		setFilter(true);
 	};
-	clear = () => {
-		this.setState({ showFiltere: false });
+	const clear = () => {
+		// this.setState({ showFiltere: false });
+		setFilter(false);
 	};
 
-	handleLogout = () => {
+	const handleLogout = () => {
 		localStorage.removeItem("token");
 		localStorage.clear();
 		window.location.reload();
 	};
 
-	render() {
-		const { users } = this.props.users;
+	const users = useSelector((state) => state.usersReducer.users);
 
-		const filtered =
-			this.props.users &&
-			this.props.users.users.filter(function (u) {
-				return (
-					u.age >= 20 &&
-					u.age < 30 &&
-					u.firstName.length + u.lastName.length >= 10
-				);
-			});
+	const filtered =
+		users &&
+		users.filter(function (u) {
+			return (
+				u.age >= 20 &&
+				u.age < 30 &&
+				u.firstName.length + u.lastName.length >= 10
+			);
+		});
 
-		return (
-			<div style={{ textAlign: "center" }}>
-				<h2>User Data</h2>
-				<div style={{ display: "flex", justifyContent: "center" }}>
-					<button onClick={this.handleLogout} className="btn">
-						Logout
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h2>User Data</h2>
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<button onClick={handleLogout} className="btn">
+					Logout
+				</button>
+				{!showFiltere && (
+					<button onClick={filtere} className="btn">
+						Filter
 					</button>
-					{!this.state.showFiltere && (
-						<button onClick={this.filtere} className="btn">
-							Filter
-						</button>
-					)}
-					{this.state.showFiltere && (
-						<button
-							onClick={this.clear}
-							className="btn"
-							style={{ border: "1px solid #888" }}
-						>
-							Clear
-						</button>
-					)}
-				</div>
-
-				{users && !this.state.showFiltere && <CardList data={users} />}
-				{users && this.state.showFiltere && <CardList data={filtered} />}
-				{users && users.length <= 0 && (
-					<div style={{ textAlign: "center" }}>
-						<h6>Loading ....</h6>
-					</div>
+				)}
+				{showFiltere && (
+					<button
+						onClick={clear}
+						className="btn"
+						style={{ border: "1px solid #888" }}
+					>
+						Clear
+					</button>
 				)}
 			</div>
-		);
-	}
-}
 
-const mapStateToProps = (state) => ({
-	users: state.usersReducer,
-});
+			{users && !showFiltere && <CardList data={users} />}
+			{users && showFiltere && <CardList data={filtered} />}
+			{users && users.length <= 0 && (
+				<div style={{ textAlign: "center" }}>
+					<h6>Loading ....</h6>
+				</div>
+			)}
+		</div>
+	);
+};
+
+// const mapStateToProps = (state) => ({
+// 	users: state.usersReducer,
+// });
 
 const mapDispatchToProps = (dispatch) => ({
 	getUsersSaga: () => dispatch(getUsersSaga()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(User);
+export default connect(mapDispatchToProps)(User);
